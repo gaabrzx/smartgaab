79b3d248c9b8034f920b53e9716646476a88e93e
gabriel-tr <slashnebulous02@gmail.com>
2023-10-24 20:05:43 -0300

    Designer Button

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,13 +1,14 @@
 import "../styles/components/cards.sass";
 
 const Cards = () => {
-  function redirect() {
-    window.open("https://wa.link/vd3046", "_blank");
-  }
+  const redirect = () => window.open("https://wa.link/vd3046", "_blank");
+  const designer = () =>
+    window.open("https://www.instagram.com/estudiogael/", "_blank");
 
   return (
     <section id="card">
       <button onClick={redirect}>Whatsapp</button>
+      <button onClick={designer}>Meu Designer</button>
     </section>
   );
 };
